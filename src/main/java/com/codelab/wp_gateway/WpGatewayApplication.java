5e3725bbb79ba8b6628b2358033075bfdb19d23f
lucasmoraist <luksmnt1101@gmail.com>
2025-09-22 08:41:22 -0300

    first commit

---
+++ src/main/java/com/codelab/wp_gateway/WpGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpGatewayApplication.class, args);
+	}
+
+}
